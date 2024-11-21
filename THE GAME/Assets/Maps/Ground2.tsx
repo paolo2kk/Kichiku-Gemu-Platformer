216bbb6266441d53a1101557b77e028abd9d251c
paolo2kk <gpaucayuela@gmail.com>
2024-11-21 20:11:25 +0100

    alfin

---
+++ THE GAME/Assets/Maps/Ground2.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Ground" tilewidth="64" tileheight="64" tilecount="48" columns="8">
+ <image source="Ground.png" width="544" height="416"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="24" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="26" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
